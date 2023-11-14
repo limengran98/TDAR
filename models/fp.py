8241e32a3727395d2f00eb8c3d061d84c7843ca9
limeng <65967004+limengran98@users.noreply.github.com>
2023-11-14 14:18:29 +0800

    Create fp.py

---
+++ models/fp.py
@@ -0,0 +1,202 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import k_hop_subgraph
+import numpy as np
+
+
+
+from collections import defaultdict
+from itertools import permutations
+import random
+from torch_scatter import scatter_add
+from torch_geometric.typing import Adj
+from torch_geometric.utils import get_laplacian, remove_self_loops
+
+
+def get_normalized_adjacency(edge_index, n_nodes, mode=None):
+    edge_weight = torch.ones((edge_index.size(1),), device=edge_index.device)
+    row, col = edge_index[0], edge_index[1]
+    deg = scatter_add(edge_weight, col, dim=0, dim_size=n_nodes)
+    if mode == "left":
+        deg_inv_sqrt = deg.pow_(-1)
+        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float("inf"), 0)
+        DAD = deg_inv_sqrt[row] * edge_weight
+    elif mode == "right":
+        deg_inv_sqrt = deg.pow_(-1)
+        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float("inf"), 0)
+        DAD = edge_weight * deg_inv_sqrt[col]
+    elif mode == "article_rank":
+        d = deg.mean()
+        deg_inv_sqrt = (deg+d).pow_(-1)
+        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float("inf"), 0)
+        DAD = deg_inv_sqrt[row] * edge_weight
+    else:
+        deg_inv_sqrt = deg.pow_(-0.5)
+        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float("inf"), 0)
+        DAD = deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+
+    return edge_index, DAD
+
+
+def get_propagation_matrix(edge_index: Adj, n_nodes: int, mode: str = "adj") -> torch.sparse.FloatTensor:
+    edge_index, edge_weight = get_laplacian(edge_index, num_nodes=n_nodes, normalization="sym")
+    if mode == "adj":
+        edge_index, edge_weight = remove_self_loops(edge_index, -edge_weight)
+    adj = torch.sparse.FloatTensor(edge_index, values=edge_weight, size=(n_nodes, n_nodes)).to(edge_index.device)
+    return adj
+
+
+def get_edge_index_from_y(y: torch.Tensor, know_mask: torch.Tensor = None) -> Adj:
+    nodes = defaultdict(list)
+    label_idx_iter = enumerate(y.numpy()) if know_mask is None else zip(know_mask.numpy(),y[know_mask].numpy())
+    for idx, label in label_idx_iter:
+        nodes[label].append(idx)
+    arr = []
+    for v in nodes.values():
+        arr += list(permutations(v, 2))
+    return torch.tensor(arr, dtype=torch.long).T
+
+
+def get_edge_index_from_y_ratio(y: torch.Tensor, ratio: float = 1.0) -> torch.Tensor:
+    n = y.size(0)
+    mask = []
+    nodes = defaultdict(list)
+    for idx, label in random.sample(list(enumerate(y.numpy())), int(ratio*n)):
+        mask.append(idx)
+        nodes[label].append(idx)
+    arr = []
+    for v in nodes.values():
+        arr += list(permutations(v, 2))
+    return torch.tensor(arr, dtype=torch.long).T, torch.tensor(mask, dtype=torch.long)
+
+
+def to_dirichlet_loss(attrs, laplacian):
+    return torch.bmm(attrs.t().unsqueeze(1), laplacian.matmul(attrs).t().unsqueeze(2)).view(-1).sum()
+
+
+class APA:
+    def __init__(self, edge_index: Adj, x: torch.Tensor, know_mask: torch.Tensor, is_binary=True):
+        self.edge_index = edge_index
+        self.x = x
+        self.n_nodes = x.size(0)
+        self.know_mask = know_mask
+        self.mean = 0 if is_binary else x[know_mask].mean(dim=0)
+        self.std = 1  # if is_binary else x[know_mask].std(dim=0)
+        self.out_k_init = (self.x[self.know_mask] - self.mean) / self.std
+        self.out = torch.zeros_like(self.x)
+        self.out[self.know_mask] = self.x[self.know_mask]
+        self._adj = None
+
+    @property
+    def adj(self):
+        if self._adj is None:
+            self._adj = get_propagation_matrix(self.edge_index, self.n_nodes)
+        return self._adj
+
+    def fp(self, out: torch.Tensor = None, num_iter: int = 30, **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        for _ in range(num_iter):
+            out = torch.spmm(self.adj, out)
+            out[self.know_mask] = self.out_k_init
+        return out * self.std + self.mean
+
+    def fp_analytical_solution(self, **kw) -> torch.Tensor:
+        adj = self.adj.to_dense()
+
+        assert self.know_mask.dtype == torch.int64
+        know_mask = torch.zeros(self.n_nodes, dtype=torch.bool)
+        know_mask[self.know_mask] = True
+        unknow_mask = torch.ones(self.n_nodes, dtype=torch.bool)
+        unknow_mask[self.know_mask] = False
+
+        A_uu = adj[unknow_mask][:, unknow_mask]
+        A_uk = adj[unknow_mask][:, know_mask]
+
+        L_uu = torch.eye(unknow_mask.sum()) - A_uu
+        L_inv = torch.linalg.inv(L_uu)
+
+        out = self.out.clone()
+        out[unknow_mask] = torch.mm(torch.mm(L_inv, A_uk), self.out_k_init)
+
+        return out * self.std + self.mean
+
+    def pr(self, out: torch.Tensor = None, alpha: float = 0.85, num_iter: int = 30, **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        for _ in range(num_iter):
+            out = alpha * torch.spmm(self.adj, out) + (1 - alpha) * out.mean(dim=0)
+            out[self.know_mask] = self.out_k_init
+        return out * self.std + self.mean
+
+    def ppr(self, out: torch.Tensor = None, alpha: float = 0.85, weight: torch.Tensor = None, num_iter: int = 50,
+            **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        if weight is None:
+            weight = self.mean
+        for _ in range(num_iter):
+            out = alpha * torch.spmm(self.adj, out) + (1 - alpha) * weight
+            out[self.know_mask] = self.out_k_init
+        return out * self.std + self.mean
+
+    def mtp(self, out: torch.Tensor = None, beta: float = 0.85, num_iter: int = 50, **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        for _ in range(num_iter):
+            out = torch.spmm(self.adj, out)
+            out[self.know_mask] = beta * out[self.know_mask] + (1 - beta) * self.out_k_init
+        return out * self.std + self.mean
+
+    def mtp_analytical_solution(self, beta: float = 0.85, **kw) -> torch.Tensor:
+        n_nodes = self.n_nodes
+        eta = (1 / beta - 1)
+        edge_index, edge_weight = get_laplacian(self.edge_index, num_nodes=n_nodes, normalization="sym")
+        L = torch.sparse.FloatTensor(edge_index, values=edge_weight, size=(n_nodes, n_nodes)).to_dense()
+        Ik_diag = torch.zeros(n_nodes)
+        Ik_diag[self.know_mask] = 1
+        Ik = torch.diag(Ik_diag)
+        out = (self.out - self.mean) / self.std
+        out = torch.mm(torch.inverse(L + eta * Ik), eta * torch.mm(Ik, out))
+        return out * self.std + self.mean
+
+    def umtp(self, out: torch.Tensor = None, alpha: float = 0.9, beta: float = 0.2, num_iter: int = 50,
+             **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        for _ in range(num_iter):
+            out = alpha * torch.spmm(self.adj, out) + (1 - alpha) * out.mean(dim=0)
+            out[self.know_mask] = beta * out[self.know_mask] + (1 - beta) * self.out_k_init
+        return out * self.std + self.mean
+
+    def umtp2(self, out: torch.Tensor = None, alpha: float = 0.9, beta: float = 0.2, gamma: float = 0.75,
+              num_iter: int = 20, **kw) -> torch.Tensor:
+        if out is None:
+            out = self.out
+        out = (out - self.mean) / self.std
+        for _ in range(num_iter):
+            out = gamma * (alpha * torch.spmm(self.adj, out) + (1 - alpha) * out.mean(dim=0)) + (1 - gamma) * out
+            out[self.know_mask] = beta * out[self.know_mask] + (1 - beta) * self.out_k_init
+        return out * self.std + self.mean
+
+    def umtp_analytical_solution(self, alpha: float = 0.85, beta: float = 0.70, **kw) -> torch.Tensor:
+        n_nodes = self.n_nodes
+        theta = (1 - 1 / self.n_nodes) * (1 / alpha - 1)
+        eta = (1 / beta - 1) / alpha
+        edge_index, edge_weight = get_laplacian(self.edge_index, num_nodes=n_nodes, normalization="sym")
+        L = torch.sparse.FloatTensor(edge_index, values=edge_weight, size=(n_nodes, n_nodes)).to_dense()
+        Ik_diag = torch.zeros(n_nodes)
+        Ik_diag[self.know_mask] = 1
+        Ik = torch.diag(Ik_diag)
+        L1 = torch.eye(n_nodes) * (n_nodes / (n_nodes - 1)) - torch.ones(n_nodes, n_nodes) / (n_nodes - 1)
+        out = (self.out - self.mean) / self.std
+        out = torch.mm(torch.inverse(L + eta * Ik + theta * L1), eta * torch.mm(Ik, out))
+        return out * self.std + self.mean
